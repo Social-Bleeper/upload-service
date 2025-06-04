b1fdf00bfc082c0022fbcba209451a65693afe74
bas <schipper.b@outlook.com>
2025-06-04 14:03:11 +0200

    disabled jwt

--- src/main/java/com/bleeper/upload_service/config/SecurityConfig.java
+++ src/main/java/com/bleeper/upload_service/config/SecurityConfig.java
@@ -25,9 +25,9 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/upload/**",
                                 "/actuator/**").permitAll()
-                        .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt.jwkSetUri("http://identity:8081/.well-known/jwks.json")));
+                        .anyRequest().authenticated());
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(jwt -> jwt.jwkSetUri("http://identity:8081/.well-known/jwks.json")));
         return http.build();
     }
 
